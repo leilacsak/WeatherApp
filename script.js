156781f7e218cbbcf4f19042079194a2dabf05bb
leilacsak <leilacsak@gmail.com>
2024-02-18 19:16:58 +0100

    line correction

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ async function getWeather() {
         console.error('Error fetching weather data:', error);
         alert('Error fetching weather data. Please try again later.');
     }
-    
-    document.getElementById('weather-button').addEventListener('click', getWeather);
 }
+
+document.getElementById('weather-button').addEventListener('click', getWeather);
+   
