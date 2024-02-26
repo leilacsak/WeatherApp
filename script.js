03d92a5088f38b196c39d932985b6656104c06cb
leilacsak <leilacsak@gmail.com>
2024-02-26 17:50:16 +0100

    change alert to a message

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const apiKey = 'b726ebbe00848d4e47d4d67d437621d8'; // OpenWeatherMap API key
-
 async function getWeather() {
     const city = prompt('Enter city name:');
     if (!city) return;
+
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
         const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Unable to fetch weather data. Please try again later.');
+        }
         const data = await response.json();
 
+        displayWeatherInfo(city, data);
+
+    }catch (error) {
+        console.error('Error fetching weather data:', error);
+        displayErrorMessage('Error fetching weather data. Please try again later.');
+    }
+    
+}
+
+function displayErrorMessage(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = message;
+}
+  
+function displayWeatherInfo(city,data) {
         const weatherInfo = document.getElementById('weather-info');
         weatherInfo.innerHTML = `
             <h2>Weather in ${city}</h2>
@@ -18,15 +36,8 @@ async function getWeather() {
             <p>Humidity: ${data.main.humidity}%</p>
             <p>Wind Speed: ${data.wind.speed} m/s</p>
         `;
-
         const weatherButton = document.getElementById('weather-button');
         weatherButton.innerText = 'Get new weather';
-
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
-        alert('Error fetching weather data. Please try again later.');
-    }
 }
 
-document.getElementById('weather-button').addEventListener('click', getWeather);
-   
+document.getElementById('weather-button').addEventListener('click', getWeather);      
